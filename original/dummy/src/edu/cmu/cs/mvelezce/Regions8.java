7f21ffe4e9f96f68a740a408baf6e0e06c8a79a2
Miguel Velez <miguelvelez@mijecu25.com>
2017-10-09 09:13:20 -0400

    Added classes

---
+++ original/dummy/src/edu/cmu/cs/mvelezce/Regions8.java
@@ -0,0 +1,29 @@
+package edu.cmu.cs.mvelezce;
+
+import edu.cmu.cs.mvelezce.analysis.option.Sink;
+import edu.cmu.cs.mvelezce.analysis.option.Source;
+
+public class Regions8 {
+
+    static boolean A = Source.getOptionA(true);
+    static boolean B = Source.getOptionB(true);
+
+    public static void main(String[] args) {
+        Sink.init();
+
+        if(A) { // A
+            foo();
+        }
+
+        if(A) { // A
+            foo();
+        }
+    }
+
+    private static void foo() {
+        if(B) { // AB
+            System.out.println();
+        }
+    }
+
+}
