8ec32ad4055e6f375d484f908b256d9fd7e6e66e
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-13 14:01:23 -0500

    Fixed path

--- original/sort/src/main/java/org/unix4j/sort/Main.java
+++ original/sort/src/main/java/org/unix4j/sort/Main.java
@@ -89,8 +89,8 @@ public class Main {
                 isDictionaryOrder, isIgnoreCase, isNumericSort,
                 isGeneralNumericSort, isHumanNumericSort, isMonthSort,
                 isVersionSort, isReverse);
-        sortArgs.setPaths(System.getProperty("user.home") + "/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/sort/files/*.txt");
-//        sortArgs.setPaths(System.getProperty("user.home") + "/Documents/performance-mapper-evaluation/original/sort/files/*.txt");
+//        sortArgs.setPaths(System.getProperty("user.home") + "/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/sort/files/*.txt");
+        sortArgs.setPaths(System.getProperty("user.home") + "/Documents/performance-mapper-evaluation/original/sort/files/*.txt");
         SortCommand sort = new SortCommand(sortArgs);
 
         DefaultExecutionContext executionContext = new DefaultExecutionContext();
