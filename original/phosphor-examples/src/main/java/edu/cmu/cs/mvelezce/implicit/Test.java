5a951c235697d38c603fb293efb13bfa7b1eb103
Miguel Velez <miguelvelez@mijecu25.com>
2018-09-20 11:38:55 -0400

    Tested sinks with phosphor

--- original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/implicit/Test.java
+++ original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/implicit/Test.java
@@ -5,21 +5,32 @@ import edu.cmu.cs.mvelezce.taints.Sources;
 
 public class Test {
 
-//  public static void main(String[] args) {
-//    testExample2();
-//  }
-//
-//  public static void testExample2() {
-//    boolean A = Sources.A_0(true);
-//    boolean B = Sources.B_1(true);
-//    boolean x = false;
-//
-//    if (A) {
-//      if (B) {
-//        x = true;
-//      }
-//    }
-//
-//    Sinks.sink(x);
-//  }
+  public static void main(String[] args) {
+    testExample2();
+  }
+
+  public static void testExample2() {
+    boolean A = Sources.A_0(true);
+    boolean B = Sources.B_1(true);
+
+    boolean x = false;
+    String s = "Miguel";
+    long l = 0L;
+    Integer i = 0;
+
+    if (A) {
+      s = "Laura";
+      x = true;
+      if (B) {
+        l = 1L;
+        i = 1;
+      }
+    }
+
+    Sinks.sink(x);
+    Sinks.sink(s);
+    Sinks.sink(s);
+    Sinks.sink(l);
+    Sinks.sink(i);
+  }
 }
