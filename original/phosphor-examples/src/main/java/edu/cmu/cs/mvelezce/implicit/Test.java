25c019019129f952ccfa6162fe3b4a65b81fd6a5
Miguel Velez <miguelvelez@mijecu25.com>
2018-09-21 12:33:51 -0400

    tested automatically adding sinks

--- original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/implicit/Test.java
+++ original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/implicit/Test.java
@@ -5,20 +5,21 @@ import edu.cmu.cs.mvelezce.taints.Sources;
 public class Test {
 
   public static void main(String[] args) {
-    testExample1();
-    //    testExample2();
+//    testExample1();
+//    testExample2();
+    testExample3();
   }
 
   public static void testExample1() {
     boolean A = Sources.A_0(true);
-    String s1 = "Miguel";
+    String s1 = "example1";
 
     String s = null;
     int i = 100;
 
     if (A) { // if<cond>
       i = 1000;
-      s = "Miguel";
+      s = "example1";
     }
 
     if (s != null) { // ifnonnull || ifnull
@@ -32,35 +33,63 @@ public class Test {
     if (i < 500) { // if_icmp<cond>
       System.out.println(s);
     }
-    //      Sinks.sink(A);
   }
-  //  public static void testExample2() {
-  //    boolean A = Sources.A_0(true);
-  //    boolean B = Sources.B_1(true);
-  //
-  //    boolean x = false;
-  //    String s = "Miguel";
-  //    long l = 0L;
-  //    int i = 0;
-  //
-  //    if (A) {
-  //      x = true;
-  //      if (B) {
-  //        l = 1L;
-  //      }
-  //    }
-  //
-  //    if(x) {
-  //      i = 1;
-  //    }
-  //
-  //    if (l > 0) {
-  //      s = "Laura";
-  //    }
-  //
-  //    Sinks.sink(x);
-  //    Sinks.sink(s);
-  //    Sinks.sink(l);
-  //    Sinks.sink(i);
-  //  }
+
+  public static void testExample2() {
+    boolean A = Sources.A_0(false);
+    String s1 = "example2";
+
+    String s = null;
+    int i = 100;
+
+    if (A) { // {A}
+      i = 1000;
+      s = "example2";
+    }
+
+    if (s != null) {
+      System.out.println(s);
+    }
+
+    if (s == s1) {
+      System.out.println(s);
+    }
+
+    if (i < 500) {
+      System.out.println(s);
+    }
+  }
+
+  public static void testExample3() {
+    boolean A = Sources.A_0(true);
+    boolean B = Sources.B_1(true);
+
+    boolean x = false;
+    String s = "example3";
+    long l = 0L;
+    int i = 0;
+
+    if (A) { // {A}
+      x = true;
+      if (B) { // {A,B}
+        l = 1L;
+      }
+    }
+
+    if (x) { // {A}
+      i = 1;
+    }
+
+    if (l > 0) { // {A, B}
+      s = "not example3";
+    }
+
+    if(s.equals("example3")) { // {A, B}
+      System.out.println();
+    }
+
+    if(i == 0) { // {A}
+      System.out.println();
+    }
+  }
 }
