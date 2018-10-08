016549853d935a16d3798762a63f0d98513a2708
Miguel Velez <miguelvelez@mijecu25.com>
2018-10-08 13:54:49 -0400

    set up to do bf for lucene

---
+++ original/lucene/demo/src/main/java/edu/cmu/cs/mvelezce/analysis/option/Source.java
@@ -0,0 +1,20 @@
+package edu.cmu.cs.mvelezce.analysis.option;
+
+public class Source {
+
+  public static boolean CREATE_0(boolean option) {
+    return option;
+  }
+
+  public static boolean RAM_BUFFER_SIZE_1(boolean option) {
+    return option;
+  }
+
+  public static boolean MERGE_POLICY_2(boolean option) {
+    return option;
+  }
+
+  public static boolean CODEC_3(boolean option) {
+    return option;
+  }
+}
