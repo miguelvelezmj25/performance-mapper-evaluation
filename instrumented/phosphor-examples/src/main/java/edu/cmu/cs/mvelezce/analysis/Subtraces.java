2e95abb746269db714e0b974751f48aaf64cdf18
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-06 13:26:48 -0400

    updated classes to instrument for subtrace analysis

--- instrumented/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Subtraces.java
+++ instrumented/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Subtraces.java
@@ -1,36 +1,43 @@
 package edu.cmu.cs.mvelezce.analysis;
 
-import edu.cmu.cs.mvelezce.taints.Sources;
-
 public class Subtraces {
 
-  public static void main(String[] args) {
-    boolean A = Sources.A_0(Boolean.valueOf(args[0]));
-    boolean B = Sources.B_1(Boolean.valueOf(args[1]));
-    boolean C = Sources.C_2(Boolean.valueOf(args[2]));
+  public static void main(String[] args) throws InterruptedException {
+    boolean A = Boolean.parseBoolean(args[0]);
+    boolean B = Boolean.parseBoolean(args[1]);
+    boolean C = Boolean.parseBoolean(args[2]);
 
     int x = 0;
 
     if (A) {
-      System.out.println(1);
+      int time = 1000;
+      System.out.println(time);
+      Thread.sleep(time);
       x = 1;
       foo(C);
     }
 
     if (B) {
-      System.out.println(2);
+      int time = 2000;
+      System.out.println(time);
+      Thread.sleep(time);
       x = 2;
       foo(C);
     }
 
     if (x > 0) {
-      System.out.println(3);
+      int time = 3000;
+      System.out.println(time);
+      Thread.sleep(time);
     }
+
   }
 
-  private static void foo(boolean x) {
+  private static void foo(boolean x) throws InterruptedException {
     if (x) {
-      System.out.println(4);
+      int time = 4000;
+      System.out.println(time);
+      Thread.sleep(time);
     }
   }
 }
--- instrumented/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Trivial.java
+++ instrumented/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Trivial.java
@@ -1,22 +1,21 @@
 package edu.cmu.cs.mvelezce.analysis;
 
-import edu.cmu.cs.mvelezce.cc.Sinks;
-import edu.cmu.cs.mvelezce.taints.Sources;
-
 public class Trivial {
 
-  public static void main(String[] args) {
-    Sinks.preProcessSinks("Trivial");
-
-    boolean A = Sources.A_0(Boolean.valueOf(args[0]));
-    boolean B = Sources.B_1(Boolean.valueOf(args[1]));
+  public static void main(String[] args) throws InterruptedException {
+    boolean A = Boolean.parseBoolean(args[0]);
+    boolean B = Boolean.parseBoolean(args[1]);
 
     if (A) {
-      System.out.println();
+      int time = 1000;
+      System.out.println(time);
+      Thread.sleep(time);
     }
 
     if (B) {
-      System.out.println();
+      int time = 2000;
+      System.out.println(time);
+      Thread.sleep(time);
     }
   }
 }
