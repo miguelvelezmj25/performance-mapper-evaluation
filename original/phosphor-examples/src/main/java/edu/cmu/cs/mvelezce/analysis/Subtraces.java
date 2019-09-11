4fadd68dd175e496f5e879fd70b1eddab4271f02
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-11 16:35:00 -0400

    updated the original files

--- original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Subtraces.java
+++ original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Subtraces.java
@@ -1,41 +1,43 @@
 package edu.cmu.cs.mvelezce.analysis;
 
-import edu.cmu.cs.mvelezce.cc.Sinks;
-import edu.cmu.cs.mvelezce.taints.Sources;
-
 public class Subtraces {
 
   public static void main(String[] args) throws InterruptedException {
-    Sinks.preProcessSinks(Subtraces.class.getSimpleName());
-
-    boolean A = Sources.A_0(Boolean.valueOf(args[0]));
-    boolean B = Sources.B_1(Boolean.valueOf(args[1]));
-    boolean C = Sources.C_2(Boolean.valueOf(args[2]));
+    boolean A = Boolean.parseBoolean(args[0]);
+    boolean B = Boolean.parseBoolean(args[1]);
+    boolean C = Boolean.parseBoolean(args[2]);
 
     int x = 0;
 
     if (A) {
-//      Thread.sleep(1000);
+      int time = 1000;
+      System.out.println(time);
+      Thread.sleep(time);
       x = 1;
       foo(C);
     }
 
     if (B) {
-//      Thread.sleep(2000);
+      int time = 2000;
+      System.out.println(time);
+      Thread.sleep(time);
       x = 2;
       foo(C);
     }
 
     if (x > 0) {
-//      Thread.sleep(3000);
+      int time = 3000;
+      System.out.println(time);
+      Thread.sleep(time);
     }
 
-    Sinks.postProcessSinks();
   }
 
   private static void foo(boolean x) throws InterruptedException {
     if (x) {
-//      Thread.sleep(4000);
+      int time = 4000;
+      System.out.println(time);
+      Thread.sleep(time);
     }
   }
 }
--- original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Trivial.java
+++ original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Trivial.java
@@ -1,15 +1,10 @@
 package edu.cmu.cs.mvelezce.analysis;
 
-import edu.cmu.cs.mvelezce.cc.Sinks;
-import edu.cmu.cs.mvelezce.taints.Sources;
-
 public class Trivial {
 
   public static void main(String[] args) throws InterruptedException {
-    Sinks.preProcessSinks("Trivial");
-
-    boolean A = Sources.A_0(Boolean.valueOf(args[0]));
-    boolean B = Sources.B_1(Boolean.valueOf(args[1]));
+    boolean A = Boolean.parseBoolean(args[0]);
+    boolean B = Boolean.parseBoolean(args[1]);
 
     if (A) {
       int time = 1000;
@@ -23,6 +18,5 @@ public class Trivial {
       Thread.sleep(time);
     }
 
-    Sinks.postProcessSinks();
   }
 }
