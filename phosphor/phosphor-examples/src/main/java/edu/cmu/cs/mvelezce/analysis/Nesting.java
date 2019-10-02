1d61ab36bdffd3faabd5ae2b72512262c5f11c83
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-02 09:50:53 -0400

    instrumented examples with phosphor

--- phosphor/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Nesting.java
+++ phosphor/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/Nesting.java
@@ -1,20 +1,24 @@
 package edu.cmu.cs.mvelezce.analysis;
 
+import edu.cmu.cs.mvelezce.cc.Sinks;
 import edu.cmu.cs.mvelezce.taints.Sources;
 
 public class Nesting {
 
   public static void main(String[] args) {
+    Sinks.preProcessSinks(Nesting.class.getSimpleName());
+
     boolean A = Sources.A_0(Boolean.valueOf(args[0]));
     boolean B = Sources.B_1(Boolean.valueOf(args[1]));
 
     if (A) {
       if (B) {
         System.out.println();
-      }
-      else {
+      } else {
         System.out.println();
       }
     }
+
+    Sinks.postProcessSinks();
   }
 }
