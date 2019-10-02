0cf7f1ad9b152ec6d12573d133b981e383c64ff7
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-02 10:44:44 -0400

    updated the options of an example

--- original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/IGenExample.java
+++ original/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/analysis/IGenExample.java
@@ -3,19 +3,19 @@ package edu.cmu.cs.mvelezce.analysis;
 public class IGenExample {
 
   public static void main(String[] args) {
-    boolean S = Boolean.parseBoolean(args[0]);
-    boolean T = Boolean.parseBoolean(args[1]);
-    boolean U = Boolean.parseBoolean(args[2]);
-    boolean V = Boolean.parseBoolean(args[3]);
-    boolean X = Boolean.parseBoolean(args[4]);
-    boolean Y = Boolean.parseBoolean(args[5]);
-    boolean Z = Boolean.parseBoolean(args[6]);
+    boolean A = Boolean.parseBoolean(args[0]);
+    boolean B = Boolean.parseBoolean(args[1]);
+    boolean C = Boolean.parseBoolean(args[2]);
+    boolean D = Boolean.parseBoolean(args[3]);
+    boolean E = Boolean.parseBoolean(args[4]);
+    boolean F = Boolean.parseBoolean(args[5]);
+    boolean G = Boolean.parseBoolean(args[6]);
 
-    int z = Z ? 4 : 0;
+    int z = G ? 4 : 0;
 
     int max_z = 3;
 
-    if (X && Y) {
+    if (E && F) {
       System.out.println("L0");
 
       if (!(0 < z && z < max_z)) {
@@ -27,10 +27,10 @@ public class IGenExample {
 
     System.out.println("L3");
 
-    if (U && V) {
+    if (C && D) {
       System.out.println("L4");
 
-      if (S || T) {
+      if (A || B) {
         System.out.println("L5");
       }
     }
