372aa439a49110753c59764881fa841ebb9b47bc
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-10 12:25:21 -0500

    added options to lucene

---
+++ original/lucene/lucene/src/main/java/edu/cmu/cs/mvelezce/cc/executor/CCIndexFiles.java
@@ -0,0 +1,23 @@
+package edu.cmu.cs.mvelezce.cc.executor;
+
+import org.apache.lucene.demo.IndexFiles;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CCIndexFiles {
+
+  public static void main(String[] args) throws IOException {
+    long start = System.nanoTime();
+    IndexFiles.main(args);
+    long end = System.nanoTime();
+    String time = (end - start) + "";
+
+    File outputFile = new File("data.ser");
+    FileOutputStream fos = new FileOutputStream(outputFile);
+    fos.write(time.getBytes());
+    fos.flush();
+    fos.close();
+  }
+}
