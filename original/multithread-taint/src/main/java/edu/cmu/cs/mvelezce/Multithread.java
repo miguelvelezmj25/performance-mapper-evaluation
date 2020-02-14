3e300f4b63a8f803d0b2301a5b815fe3960bc271
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-14 16:53:35 -0500

    added multithreaded program

---
+++ original/multithread-taint/src/main/java/edu/cmu/cs/mvelezce/Multithread.java
@@ -0,0 +1,61 @@
+package edu.cmu.cs.mvelezce;
+
+import edu.cmu.cs.mvelezce.worker.PrimeNumberChecker;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Multithread {
+  public static void main(String[] args) {
+    boolean A = Boolean.parseBoolean(args[0]);
+    boolean B = Boolean.parseBoolean(args[1]);
+    boolean C = Boolean.parseBoolean(args[2]);
+
+    multipleCalss();
+    singleCall(A, B, C);
+  }
+
+  private static void singleCall(boolean a, boolean b, boolean c) {
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    executorService.execute(new PrimeNumberChecker(a));
+    executorService.execute(new PrimeNumberChecker(b));
+    executorService.execute(new PrimeNumberChecker(c));
+    executorService.shutdown();
+
+    while (!executorService.isTerminated()) {
+      try {
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        //
+      }
+    }
+  }
+
+  private static void multipleCalss() {
+    //    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    //    executorService.execute(new PrimeNumberChecker(200_000));
+    //    executorService.shutdown();
+    //
+    //    while (!executorService.isTerminated()) {
+    //      try {
+    //        executorService.awaitTermination(1, TimeUnit.SECONDS);
+    //      } catch (InterruptedException e) {
+    //        //
+    //      }
+    //    }
+    //
+    //    executorService = Executors.newFixedThreadPool(2);
+    //    executorService.execute(new PrimeNumberChecker(400_000));
+    //    executorService.execute(new PrimeNumberChecker(700_000));
+    //    executorService.shutdown();
+    //
+    //    while (!executorService.isTerminated()) {
+    //      try {
+    //        executorService.awaitTermination(1, TimeUnit.SECONDS);
+    //      } catch (InterruptedException e) {
+    //        //
+    //      }
+    //    }
+  }
+}
---
+++ original/multithread-taint/src/main/java/edu/cmu/cs/mvelezce/worker/PrimeNumberChecker.java
@@ -0,0 +1,34 @@
+package edu.cmu.cs.mvelezce.worker;
+
+public class PrimeNumberChecker implements Runnable {
+
+  private final int maxNumberToCheck;
+
+  public PrimeNumberChecker(boolean numberToCheck) {
+    if (numberToCheck) {
+      this.maxNumberToCheck = 300_000;
+    } else {
+      this.maxNumberToCheck = 800_000;
+    }
+  }
+
+  @Override
+  public void run() {
+    System.out.println(Thread.currentThread() + " started");
+
+    for (int numberToCheck = 1; numberToCheck <= this.maxNumberToCheck; numberToCheck++) {
+      isPrime(numberToCheck);
+    }
+
+    System.out.println(Thread.currentThread() + " done");
+  }
+
+  private void isPrime(int numberToCheck) {
+    for (int i = 2; i <= numberToCheck / 2; i++) {
+      int remainder = numberToCheck % i;
+      if (remainder == 0) {
+        return;
+      }
+    }
+  }
+}
