798b4e852a1eb8a659297558f72b1cd5a440717e
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-24 14:14:06 -0500

    replicated performance test in a simple main method

---
+++ original/h2database/src/main/edu/cmu/cs/mvelezce/bench/RunBenchC.java
@@ -0,0 +1,35 @@
+package edu.cmu.cs.mvelezce.bench;
+
+import edu.cmu.cs.mvelezce.bench.c.BenchC;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+public class RunBenchC {
+
+  private static final String DB_STRING = "H2, org.h2.Driver, jdbc:h2:./data/test, sa, sa";
+  //  private static final int SIZE = 5000;
+  private static final int SIZE = 1;
+
+  public static void main(String[] args) throws Exception {
+    Database db = Database.parse(DB_STRING, 1);
+    System.out.println();
+    System.out.println("Testing the performance of " + db.getName());
+    Connection conn = db.openNewConnection();
+    DatabaseMetaData meta = conn.getMetaData();
+    System.out.println(
+        " " + meta.getDatabaseProductName() + " " + meta.getDatabaseProductVersion());
+
+    Bench bench = new BenchC();
+    bench.init(db, SIZE);
+    bench.runTest();
+    conn.close();
+
+    db.log("Executed statements", "#", db.getExecutedStatements());
+    db.log("Total time", "ms", db.getTotalTime());
+    int statPerSec = (int) (db.getExecutedStatements() * 1000L / db.getTotalTime());
+    db.log("Statements per second", "#", statPerSec);
+    System.out.println("Statements per second: " + statPerSec);
+    System.out.println("GC overhead: " + (100 * db.getTotalGCTime() / db.getTotalTime()) + "%");
+  }
+}
