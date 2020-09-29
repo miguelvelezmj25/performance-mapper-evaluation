17fa784658c72f4c23e3e4a7993ee038fff563cf
Miguel Velez <miguelvelez@mijecu25.com>
2020-09-29 11:06:58 -0400

    added simple programs to analyze

---
+++ phosphor/phosphor-examples/src/main/java/edu/cmu/cs/mvelezce/perf/DummyRegion.java
@@ -0,0 +1,52 @@
+package edu.cmu.cs.mvelezce.perf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DummyRegion {
+
+  private static final List<String> LIST = new ArrayList<>();
+
+  public static void main(String[] args) {
+    boolean A = Boolean.parseBoolean(args[0]);
+    foo(A);
+  }
+
+  private static void foo(boolean x) {
+    int bound;
+    if (x) {
+      bound = 50_000_000;
+    } else {
+      bound = 500_000_000;
+    }
+    bar(bound);
+  }
+
+  private static void bar(int bound) {
+    start();
+    for (int i = 0; i < bound; i++) {
+      moo(bound);
+    }
+    end();
+  }
+
+  private static void moo(int bound) {
+    if (bound > 0) { }
+    ExpensiveCalls.getFactors();
+    checkProgress();
+  }
+
+  private static void end() {
+    System.out.println("END");
+  }
+
+  private static void start() {
+    System.out.println("START");
+  }
+
+  private static void checkProgress() {
+    for (String s : LIST) {
+      System.out.println(s);
+    }
+  }
+}
